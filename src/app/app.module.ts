f4eeb97f6f5b386365653b8d1b3d5795e6172442
haribabu-y <haribabuitorizon@gmail.com>
2025-05-21 22:28:46 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from './header/header.component';
 
 import { AppComponent } from './app.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopHeaderComponent,
+    TopMenuComponent,
+    MainMenuComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/top-header/top-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'top-header',
+  // templateUrl: './top-header.component.html',
+  template: '<p>Top Header</p>',
+  styleUrls: ['./top-header.component.css']
+})
+export class TopHeaderComponent {
+  // title = "My First Angular App";
+}
